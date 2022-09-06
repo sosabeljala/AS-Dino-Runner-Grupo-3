d032e35f623081ac2af1e2955067f70ec8a2a8f2
Abel Sossa <Abel.Sossa@fundacion-jala.org>
2022-09-06 18:17:04 -0400

    Update dinosaur.py

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -31,13 +31,13 @@ class Dinosaur:
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False 
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
-
-        if user_input[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
-            self.dino_run = False 
+    
         
 
         if self.step_index >= 10:
@@ -64,9 +64,9 @@ class Dinosaur:
         self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
-        self.dino_rect.y = 350
+        self.dino_rect.y = self.Y_POS + 40
         self.step_index +=1
-        ##fin
+    
        
 
 
